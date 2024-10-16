dda6416d101db72a0825133bcb95ddbbdcfb9cda
Joss-inf <jossbessi460@gmail.com>
2024-10-16 09:33:22 +0200

    addingParser

---
+++ src/parser.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+"""
+Construis le chemin complet vers le fichier JSON
+"""
+json_file_path = os.path.join(current_dir, '../data/config.json')
+"""
+Obtiens le chemin absolu du répertoire courant
+"""
+
+try:
+    with open(json_file_path,"r") as f:
+        data = json.load(f)
+except FileNotFoundError:
+    print("Le fichier config.json n'a pas été trouvé.")
+except json.JSONDecodeError:
+    print("Le fichier config.json contient des erreurs de syntaxe JSON.")
+"""
+On charge le Json avec un try et des retours d'erreur
+"""
+def GetJsonParse():
+    return data
