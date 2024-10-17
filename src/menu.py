acced9efe17a8dd92c3c7cab1f568056bf2fcc23
Joss-inf <jossbessi460@gmail.com>
2024-10-17 22:53:51 +0200

    fix and rename command to menu

---
+++ src/menu.py
@@ -0,0 +1,46 @@
+import init
+import loading as l
+import copy
+
+def command():
+    while True:
+        txt = "new party : 0, continue :1, record : 2, leaderboard : 3 input: "
+        a = userInput(txt)
+        if a == 0:
+            game,player= init.init()
+            player1 = copy.deepcopy(player)
+            player2 = copy.deepcopy(player)
+            """
+            deep copy  pour creer un nouvel objet unique avec sa propre adresse sinon c'est la meme adresse
+            """
+            player1.name = "player1"
+            player2.name = "player2"
+            """
+                chargement de la config des bateaux
+            """
+            l.LoadShip(player1,game)
+            l.LoadShip(player2,game)
+
+            if a == 1:
+                 #LoadLastGame()
+                i = 1
+            if a == 2:
+                 #LoadRecord()
+                i = 1
+            if a == 3:
+                i = 1
+                #LoadLeaderBoard()
+
+
+def userInput(str1: str):
+    """
+    validation de  l input entre 0 et 3
+    si pas un int error print
+    """
+    n:int = -1
+    while n < 0  or n > 3:
+        try:
+            res = int(input(str1))
+            return res
+        except ValueError:
+            print('Must be a valid input')
